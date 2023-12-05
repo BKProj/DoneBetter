8e4288cf35aabf4e06cd77c5c0d20e4cb8d0e152
BNK <91459350+BNKLovesTrollin@users.noreply.github.com>
2023-12-05 09:37:55 +0330

    Update schedule.js

--- watch/schedule.js
+++ watch/schedule.js
@@ -74,7 +74,7 @@ function displayFollowingSlots(scheduleData, nearestSlot, showsData) {
 
 // Function to set the background image for each show block
 function setBackgroundForShowBlock(showBlock, showId) {
-    const imagePath = `https://donebetter.live/images/${showId}.png?${Date.now()}`; // Add random query parameter
+    const imagePath = `/images/${showId}.png?${Date.now()}`; // Add random query parameter
     const placeholderImg = '/images/placeholder.png'
 
     // Check if the image exists
